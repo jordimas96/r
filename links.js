d0b7ef4fefb15878c96aefa51b9f5e82e452b43e
Jordi Mas Parramon <xordimas@gmail.com>
2024-10-11 03:18:15 +0200

    Move links to an external file Add green color wen key is correct Trim text to compare

---
+++ links.js
@@ -0,0 +1,60 @@
+/*
+            
+EXEMPLE:
+
+https://jordimas96.github.io/r/play-store
+
+*/
+
+const redireccions = {
+    // CV //
+    "cv ca": "https://jordimas96.github.io/assets/documents/CV/CV%20Jordi%20Mas%20Parramon%20CA.pdf",
+    "cv es": "https://jordimas96.github.io/assets/documents/CV/CV%20Jordi%20Mas%20Parramon%20ES.pdf",
+    "cv en": "https://jordimas96.github.io/assets/documents/CV/CV%20Jordi%20Mas%20Parramon%20EN.pdf",
+    
+    // Al CV //
+    "pdf daw montilivi": "https://www.institutmontilivi.cat/articles/oferta-formativa/cicles-formatius-grau-superior-bottom/D%C3%ADptics%202023/DAW_10_mar%C3%A7_2023.pdf",
+    "pdf dam montilivi": "https://www.institutmontilivi.cat/articles/oferta-formativa/cicles-formatius-grau-superior-bottom/D%C3%ADptics%202023/DAM_10_mar%C3%A7_2023.pdf",
+    // https://www.institutmontilivi.cat/oferta-formativa/cicles-formatius-grau-superior/ //
+
+
+
+    // Apps //
+    "play store":       "https://jordimas96.github.io/external-pages/play-store/play-store.html", // Clone from https://play.google.com/store/apps/developer?id=Jordi+Mas+Parramon (inactive)
+    "amazon appstore":  "https://www.amazon.com/s?rh=p_4%3AJMasDev",
+
+
+    // Links set in my Android Apps //
+    "play store from apps":             "https://jordimas96.github.io/r/play-store",
+    // "app info from breaking ball":      "https://jordimas96.github.io/external-pages/play-store/apps/breaking-ball.html", // No serà utlilitzat, ja no tinc el projecte //
+    // "app info from sus tracker":        "https://jordimas96.github.io/external-pages/play-store/apps/sus-tracker.html", // No utlilitzat //
+    "app info from wallpaper blurrer":  "https://jordimas96.github.io/external-pages/play-store/apps/wallpaper-blurrer.html",
+    "app info from text mimicker":      "https://jordimas96.github.io/external-pages/play-store/apps/text-mimicker.html",
+
+
+    // APKs //
+    "breaking ball":        "https://jordimas96.github.io/external-pages/play-store/apks/Breaking Ball 3.4.apk",
+    "sus tracker":          "https://jordimas96.github.io/external-pages/play-store/apks/SUS Tracker 3.0.1.apk",
+    "wallpaper blurrer":    "https://jordimas96.github.io/external-pages/play-store/apks/Wallpaper Blurrer 1.3.apk",
+    "text mimicker":        "https://jordimas96.github.io/external-pages/play-store/apks/Text Mimicker 1.1.apk",
+
+
+    "mad jumpgate": "https://jordimas96.github.io/external-pages/mad-jumpgate",
+
+
+    "a cantar chicos": "https://open.spotify.com/playlist/0dFUSBV3XBp8UATBeu6nYv?si=bc33fa9a275941b4",
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+};
