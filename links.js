4f6bd6feaba4a54a3ba50f74230e42f59dbcf7c3
Jordi Mas Parramon <xordimas@gmail.com>
2025-03-17 00:44:01 +0100

    Repaired always down link

--- links.js
+++ links.js
@@ -51,9 +51,10 @@ const redireccions = {
     "a cantar chicos": "https://open.spotify.com/playlist/0dFUSBV3XBp8UATBeu6nYv?si=bc33fa9a275941b4",
 
 
-    // "always down": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "always down": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     "always down": "https://www.yout-ube.com/watch?v=dQw4w9WgXcQ",
-    // "always down": assets_repo + "/alwaysdown.mp4",
+    // "always down": "https://github.com/jordimas96/assets_repo/raw/refs/heads/main/alwaysdown.mp4",
+    "always down": assets_repo + "/alwaysdown.mp4",
 
 
 
