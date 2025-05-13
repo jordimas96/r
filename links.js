eb4660f69139602356eaca5b3095d60d24077edd
Jordi Mas Parramon <xordimas@gmail.com>
2025-05-13 23:23:13 +0200

    New link: Wallpaper Blurrer play store app page

--- links.js
+++ links.js
@@ -46,8 +46,10 @@ const redireccions = {
 
 
     "mad jumpgate": assets_repo + "/jordimas96.github.io/external-pages/mad-jumpgate",
+
     "fps from instagram story": "https://jordimas96.github.io/section/mad-jumpgate",
     "sus tracker play store from instagram story": assets_repo + "/jordimas96.github.io/external-pages/play-store/apps/sus-tracker.html",
+    "wallpaper blurrer play store from instagram story": assets_repo + "/jordimas96.github.io/external-pages/play-store/apps/wallpaper-blurrer.html",
 
 
     "a cantar chicos": "https://open.spotify.com/playlist/0dFUSBV3XBp8UATBeu6nYv?si=bc33fa9a275941b4",
